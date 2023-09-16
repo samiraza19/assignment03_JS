f09c6ca07db1d2a8d006d5fa5be509bfbf76bae8
=SamiRaza <=samirazagb@gmail.com>
2023-09-16 22:57:08 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,71 @@
+// Q 1
+for (var i = 1; i <= 10; i++) {
+    console.log("welcome Sami",i);
+  }
+
+// Q 2
+for (var i = 1; i <= 12; i++) {
+    document.write(i,"Hello User" ,"<br>");
+  }
+
+// Q 3
+for (var i = 1;i <= 5; i++){
+    console.log("Correct Statement" ,i)
+}
+
+// Q 4
+for (var i = 1;i < 101;i++){
+    console.log(i,"Assalam O Alaikum")
+}
+
+// Q 5
+document.write("Q 2" +"<br>")
+for (var i = 1;i <= 3;i++){
+    document.write(i,"Hello World" + "<br>")
+}
+
+// Q 6
+var arr =["Apple","Mango","Banana","Grapes"]
+for (var i = 1;i <=1; i++){
+    console.log(arr)
+}
+
+//  Q 7
+var matchFound = false;
+for (var i = 0; i <= 4; i++);
+matchFound = true;
+alert("It's one of the cleanest cities");
+if (matchFound === false) {
+alert("It's not on the list");
+}
+
+// Q 8
+var arr =["parrot","duck","goat","cow"]
+for (var i = 1;i<= 1; i++){
+    document.write(arr +"<br>")
+}
+
+// Q 9
+for (var i = 1;i<=1;i++){
+    alert("Code practice")
+}
+
+
+
+// Q 10
+var userName = prompt("Enter your username");
+
+var arr = ["Sami raza"];
+
+for (var i = 0; i <= arr.length; i++) {
+  if (userName === "Sami raza") {
+    alert("Welcome" + " " + userName);
+  }else{
+    alert("Please enter correct username")
+  }
+}
+
+// Q 11
+var matchFound = false;
+alert("Match found");
+matchFound = true; 
